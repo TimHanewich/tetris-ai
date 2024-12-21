b4eb49400e88bcab3d157f11a173eb89bcee628d
Tim Hanewich <tahanewich@live.com>
2024-12-21 12:55:30 -0500

    Updated BoardRepresentation to use relative depth

--- src/representation.py
+++ src/representation.py
@@ -12,48 +12,14 @@ def PieceState(p:tetris.Piece) -> list[int]:
     return ToReturn
 
 def BoardState(gs:tetris.GameState) -> list[int]:
+    """Returns the relative available depth of the board."""
 
-    ToReturn:list[int] = []
+    columns_depth:list[int] = gs.column_depths()
 
-    # add board by row
-    ToReturn.append(int(gs.board[0][0]))
-    ToReturn.append(int(gs.board[0][1]))
-    ToReturn.append(int(gs.board[0][2]))
-    ToReturn.append(int(gs.board[0][3]))
-
-    ToReturn.append(int(gs.board[1][0]))
-    ToReturn.append(int(gs.board[1][1]))
-    ToReturn.append(int(gs.board[1][2]))
-    ToReturn.append(int(gs.board[1][3]))
-
-    ToReturn.append(int(gs.board[2][0]))
-    ToReturn.append(int(gs.board[2][1]))
-    ToReturn.append(int(gs.board[2][2]))
-    ToReturn.append(int(gs.board[2][3]))
-
-    ToReturn.append(int(gs.board[3][0]))
-    ToReturn.append(int(gs.board[3][1]))
-    ToReturn.append(int(gs.board[3][2]))
-    ToReturn.append(int(gs.board[3][3]))
-
-    ToReturn.append(int(gs.board[4][0]))
-    ToReturn.append(int(gs.board[4][1]))
-    ToReturn.append(int(gs.board[4][2]))
-    ToReturn.append(int(gs.board[4][3]))
-
-    ToReturn.append(int(gs.board[5][0]))
-    ToReturn.append(int(gs.board[5][1]))
-    ToReturn.append(int(gs.board[5][2]))
-    ToReturn.append(int(gs.board[5][3]))
-
-    ToReturn.append(int(gs.board[6][0]))
-    ToReturn.append(int(gs.board[6][1]))
-    ToReturn.append(int(gs.board[6][2]))
-    ToReturn.append(int(gs.board[6][3]))
-
-    ToReturn.append(int(gs.board[7][0]))
-    ToReturn.append(int(gs.board[7][1]))
-    ToReturn.append(int(gs.board[7][2]))
-    ToReturn.append(int(gs.board[7][3]))
-
-    return ToReturn
+    # subtract 1 from each until one is 0
+    # doing this allows for the depth to be "relative"
+    while 0 not in columns_depth:
+        for i in range(0, len(columns_depth)):
+            columns_depth[i] = columns_depth[i] - 1
+    
+    return columns_depth
