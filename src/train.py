9442cfcd3a972d55dcbefbfa832e5d263968ed6f
Tim Hanewich <tahanewich@live.com>
2024-12-20 22:47:37 -0500

    Simplified training code

--- src/train.py
+++ src/train.py
@@ -7,9 +7,8 @@ model_save_path = r"" # if you want to start from a checkpoint, fill this in wit
 log_file_path:str = r"C:\Users\timh\Downloads\tah\tetris-ai\checkpoints\log.txt" # during training, if you want logs to be saved in this file about the progress of performance improvemnts during training, put a path to a txt file here. Logs will be appended.
 
 # settings for training
-games_in_batch:int = 100 # how many games will be played (simulated), with the top X% being used to train
-best_game_focus:int = 10 # the top X games that will be trained on
-accrue_games_before_training:int = 100 # the number of TOP games (games that will be trained on) which will be collected before it trains on them
+games_in_episode:int = 1000 # how many games will be played (simulated), with the top X% being used to train
+train_on_best_count:int = 100 # the number of TOP games (games that will be trained on) which will be trained on out of the episode
 training_epochs:int = 30 # the number of epochs those accrued good games are trained on
 total_games:int = 100000 # the total number of games to train on. Once the model has been trained on this number, it will stop
 save_checkpoint_every_trained:int = 1000 # after training each X number of games, a checkpoint will be saved
@@ -34,51 +33,53 @@ games_trained_at_last_checkpoint:int = 0
 while games_trained < total_games:
 
     # collect games to train on by playing continuously, over and over
-    scores:list[int] = [] # all of the scores that have been played so far by this model version (in this current state, before the next training)
+    scores:list[int] = [] # all of the scores that have been played so far in this episode
     GamesToTrainOn:list[intelligence.PlayedGame] = [] # the top games that we will train on later
-    while len(GamesToTrainOn) < accrue_games_before_training:
 
-        status:str = "(" + str(games_trained) + " trained / " + str(total_games) + " goal) (" + str(len(GamesToTrainOn)) + " accrued / " + str(accrue_games_before_training) + " batch goal)"
+    # play (simulate) all of the games we are supposed to per episode
+    GameSimulations:list[intelligence.PlayedGame] = []
+    for x in range(0, games_in_episode):
 
-        # play (simulate) a small-ish batch of games
-        GameSimulations:list[intelligence.PlayedGame] = []
-        for x in range(0, games_in_batch):
-            status_line:str = "\r" + status + " " + "Simulating game # " + str(x+1) + " / " + str(games_in_batch)
-            sys.stdout.write(status_line) # write the update
-            sys.stdout.flush() # clear the current line
-            pg = intelligence.simulate_game(tai)
-            scores.append(pg.final_score) # append final score to the ongoing list
-            GameSimulations.append(pg) # add this game to the list of games played
-        print() # go to next line
+        # construct the line to write
+        status_line:str = "\r" + "(" + str(games_trained) + " trained / " + str(total_games) + " goal)" + " " + "Simulating game # " + str(x+1) + " / " + str(games_in_episode)
+        
+        # append avg score if there is at least one game yet (can't divide by 0!)
+        if len(scores) > 0:
+            avg_score_in_this_episode:float = round(sum(scores) / len(scores), 1)
+            status_line = status_line + ", avg score = " + str(avg_score_in_this_episode)
+        
+        # print
+        sys.stdout.write(status_line) # write the update
+        sys.stdout.flush() # clear the current line
 
-        # calculate and print the average score of that batch of games
-        batch_score_sum:int = 0
+        # simulate and append
+        pg = intelligence.simulate_game(tai)
+        scores.append(pg.final_score) # append final score to the ongoing list
+        GameSimulations.append(pg) # add this game to the list of games played
+    print() # go to next line
+
+    # sort that batch of games by score, highest to lowest
+    print("Sorting " + str(len(GameSimulations)) + " games by score...")
+    GameSimulationsOrdered:list[intelligence.PlayedGame] = []
+    while len(GameSimulations) > 0:
+        best:intelligence.PlayedGame = GameSimulations[0]
         for pg in GameSimulations:
-            batch_score_sum = batch_score_sum + pg.final_score
-        print("Avg. score of this episode, over " + str(len(scores)) + " games: " + str(round(sum(scores) / len(scores), 1)))
+            if pg.final_score > best.final_score:
+                best = pg
+        GameSimulationsOrdered.append(best)
+        GameSimulations.remove(best)
 
-        # sort that batch of games by score, highest to lowest
-        print("Sorting " + str(len(GameSimulations)) + " games by score...")
-        GameSimulationsOrdered:list[intelligence.PlayedGame] = []
-        while len(GameSimulations) > 0:
-            best:intelligence.PlayedGame = GameSimulations[0]
-            for pg in GameSimulations:
-                if pg.final_score > best.final_score:
-                    best = pg
-            GameSimulationsOrdered.append(best)
-            GameSimulations.remove(best)
+    # take the top X games and put them in a list that we will train on later
+    print("Selecting best " + str(train_on_best_count) + " games from this episode for training...")
+    for i in range(train_on_best_count): # take the top ones
+        GamesToTrainOn.append(GameSimulationsOrdered[i])
 
-        # take the top X games and put them in a list that we will train on later
-        print("Selecting best " + str(best_game_focus) + " games for future training...")
-        for i in range(best_game_focus): # take the top ones
-            GamesToTrainOn.append(GameSimulationsOrdered[i])
-
-        # get avg score of best group
-        score:int = 0
-        for pg in GamesToTrainOn:
-            score = score + pg.final_score
-        avg_score_best = score / len(GamesToTrainOn)
-        print("Avg. score of " + str(best_game_focus) + " BEST games from that last batch of " + str(games_in_batch) + ": " + str(round(avg_score_best,1)))
+    # get avg score of best group
+    score:int = 0
+    for pg in GamesToTrainOn:
+        score = score + pg.final_score
+    avg_score_best = score / len(GamesToTrainOn)
+    print("Avg. score of " + str(train_on_best_count) + " BEST games from that last batch of " + str(games_in_episode) + ": " + str(round(avg_score_best,1)))
     
     # if they provided a log file path, log the average performance in there
     if log_file_path != None and log_file_path != "":
