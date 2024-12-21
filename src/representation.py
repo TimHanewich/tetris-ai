1b452bdf23af6e93d36614d4fed0e31c92d0dbfa
Tim Hanewich <tahanewich@live.com>
2024-12-21 13:03:02 -0500

    Re-wrote piece representation

--- src/representation.py
+++ src/representation.py
@@ -1,15 +1,28 @@
 import tetris
 
 def PieceState(p:tetris.Piece) -> list[int]:
+    """Returns the number of spaces that are 'empty' from the bottom from the bottom up. In other words, for each column, looking from the bottom up, if the bottom-most square of that columns is occupied, that column would be 0. If it is not, it would be 1, but then if the top square is not, it would be 2. If the bottom square is occupied but the top is not occupied, it is still 0. Because at that point the top doesn't matter as long as the bottom square is occupied. If both in that column are not occoupeid, it would be 2 of course. So, it is like the 'empty squares from the bottom up' to explain it in different words."""
+
     ToReturn:list[int] = []
 
-    # add piece
-    ToReturn.append(int(p.squares[0][0]))
-    ToReturn.append(int(p.squares[0][1]))
-    ToReturn.append(int(p.squares[1][0]))
-    ToReturn.append(int(p.squares[1][1]))
+    # column A
+    if p[1][0]:
+        ToReturn.append(0)
+    elif p[0][0]:
+        ToReturn.append(1)
+    else: # if both in the first column are not on, do 2.
+        ToReturn.append(2)
 
-    return ToReturn
+    # column B
+    if p[1][1]:
+        ToReturn.append(0)
+    elif p[0][1]:
+        ToReturn.append(1)
+    else: # if both in the first column are not on, do 2.
+        ToReturn.append(2)
+
+    retrun ToReturn
+    
 
 def BoardState(gs:tetris.GameState) -> list[int]:
     """Returns the relative available depth of the board."""
