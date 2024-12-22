42d0edfafff1dfd5a13f62959e550dcce06d87cd
Tim Hanewich <tahanewich@live.com>
2024-12-22 09:32:12 -0500

    Adjusted when to train

--- src/train.py
+++ src/train.py
@@ -10,7 +10,7 @@ log_file_path:str = r"C:\Users\timh\Downloads\tah\tetris-ai\checkpoints\log.txt"
 games_in_episode:int = 300 # how many games will be played (simulated), with the top X% being used to train
 train_on_best_count:int = 30 # the number of TOP games (games that will be trained on) which will be trained on out of the episode
 training_epochs:int = 20 # the number of epochs those accrued good games are trained on
-save_checkpoint_every_trained:int = 500 # after training each X number of games, a checkpoint will be saved
+save_checkpoint_every_trained:int = 150 # after training each X number of games, a checkpoint will be saved
 
 # epsilon (exploration) settings
 epsilon_initial:float = 0.50 # the initial epsilon value to start at
