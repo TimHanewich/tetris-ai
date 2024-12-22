6cd27955ed1237edf178abecec837be4915456b2
Tim Hanewich <tahanewich@live.com>
2024-12-21 22:14:31 -0500

    Removed eval path

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,7 +1,7 @@
 import intelligence
 import tetris
 
-save_path = r"C:\Users\timh\Downloads\tah\tetris-ai\checkpoints\checkpoint0.keras"
+save_path = r""
 tai = intelligence.TetrisAI(save_path)
 
 while True:
