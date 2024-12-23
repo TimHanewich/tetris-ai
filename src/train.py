0fea39485820703e742b4ae6d42e0ded4a282cdf
Tim Hanewich <tahanewich@live.com>
2024-12-23 13:06:35 -0500

    Record and monitor game scores

--- src/train.py
+++ src/train.py
@@ -12,6 +12,9 @@ log_file_path:str = r"C:\Users\timh\Downloads\tah\tetris-ai\checkpoints\log.txt"
 # training settings
 gamma:float = 0.5
 epsilon:float = 0.2
+
+# training config
+collect_experiences:int = 100 # how many moves the model will play (across multiple games) before stopping to train
 ################
 
 
@@ -25,91 +28,103 @@ else:
     print("Constructing new model...")
     tai = intelligence.TetrisAI()  
 
-# train!
+# variables to track
 experiences:list[intelligence.Experience] = []
+GameScores:list[int] = []
+
+# train!
 gs:tetris.GameState = tetris.GameState()
-for epoch in range(0, 1000000000):
-    print("On epoch " + str(epoch) + "... ")
+while True:
 
-    # create new piece that will have to be decided on what move to play
-    p:tetris.Piece = tetris.Piece()
-    p.randomize()
+    # collect a certain number of moves
+    for i in range(0, collect_experiences):
 
-    # prepare the piece and board as a representation (state) that could be passed to the model
-    state_piece:list[int] = representation.PieceState(p)
-    state_board:list[int] = representation.BoardState(gs)
+        # prepare status
+        status:str = ""
+        if len(GameScores) > 0: status = status + "(" + str(round(sum(GameScores) / len(GameScores), 1)) + " avg score over " + str(len(GameScores)) + " games) "
+        status = status + "Collecting experience # " + str(i+1) + " / " + str(collect_experiences) + "... "
 
-    # select what move to play
-    move:int
-    if tools.oddsof(epsilon): # if, by chance (chance determined by epsilon as part of e-greedy), select a random move
-        if p.width == 4: # one shape
-            move = random.randint(0, 6)
-        elif p.width == 3: # most scenarios
-            move = random.randint(0, 7)
-        elif p.width == 2: # one shape
-            move = random.randint(0, 8) 
-    else:
-        move = tools.highest_index(tai.predict(state_piece, state_board)) # select the index of the highest value (highest perceived reward) out of the whole prediction of Q-Values.
+        # print
+        sys.stdout.write(status)
+        sys.stdout.flush()
 
-    # record the score BEFORE
-    score_before:float = gs.score_plus()
+        # create new piece that will have to be decided on what move to play
+        p:tetris.Piece = tetris.Piece()
+        p.randomize()
 
-    # play the move
-    IllegalMovePlayed:bool = False
-    try:
-        gs.drop(p, move)
-    except tetris.InvalidShiftException as ex: # the model tried to play an illegal move
-        IllegalMovePlayed = True
-    except Exception as ex:
-        print("Unhandled exception in move execution: " + str(ex))
-        input("Press enter key to continue, if you want to.")
+        # prepare the piece and board as a representation (state) that could be passed to the model
+        state_piece:list[int] = representation.PieceState(p)
+        state_board:list[int] = representation.BoardState(gs)
 
-    # record score after
-    score_after:float = gs.score_plus()
-    if IllegalMovePlayed:
-        score_after = 0.0
+        # select what move to play
+        move:int
+        if tools.oddsof(epsilon): # if, by chance (chance determined by epsilon as part of e-greedy), select a random move
+            if p.width == 4: # one shape
+                move = random.randint(0, 6)
+            elif p.width == 3: # most scenarios
+                move = random.randint(0, 7)
+            elif p.width == 2: # one shape
+                move = random.randint(0, 8) 
+        else:
+            move = tools.highest_index(tai.predict(state_piece, state_board)) # select the index of the highest value (highest perceived reward) out of the whole prediction of Q-Values.
 
-    # calculate the reward from this action
-    reward:float = score_after - score_before
+        # record the score BEFORE
+        score_before:float = gs.score_plus()
 
-    # come up with a random piece that will be used as a dummy "next piece" in the next state.
-    # since the piece generation is always random, it doesnt matter that the next piece is ACTUALLY the next piece.
-    next_piece:tetris.Piece = tetris.Piece()
-    next_piece.randomize()
+        # play the move
+        IllegalMovePlayed:bool = False
+        try:
+            gs.drop(p, move)
+        except tetris.InvalidShiftException as ex: # the model tried to play an illegal move
+            IllegalMovePlayed = True
+        except Exception as ex:
+            print("Unhandled exception in move execution: " + str(ex))
+            input("Press enter key to continue, if you want to.")
 
-    # store this scenario as an experience
-    exp:intelligence.Experience = intelligence.Experience()
-    exp.state = (state_piece, state_board)
-    exp.action = move
-    exp.reward = reward
-    exp.next_state = (representation.PieceState(next_piece), representation.BoardState(gs))
-    exp.done = gs.over()
-    experiences.append(exp)
+        # record score after
+        score_after:float = gs.score_plus()
+        if IllegalMovePlayed:
+            score_after = 0.0
 
-    # train!
-    if len(experiences) >= 32: # if we have accrued a certain batch size number
-        print("Recorded experiences has reached " + str(len(experiences)) + " experiences. Time to train!")
+        # calculate the reward from this action
+        reward:float = score_after - score_before
 
-        # train on every experience
-        for exp in experiences:
+        # come up with a random piece that will be used as a dummy "next piece" in the next state.
+        # since the piece generation is always random, it doesnt matter that the next piece is ACTUALLY the next piece.
+        next_piece:tetris.Piece = tetris.Piece()
+        next_piece.randomize()
 
-            new_target:float # "new_target" is essentially the 'correct' Q-Value that we want the Neural Network to learn for that particular state and action it did. In other words, we are going to set this to the updated current/future reward blend, plug this value into the prediction array, and then train on it.
+        # store this scenario as an experience
+        exp:intelligence.Experience = intelligence.Experience()
+        exp.state = (state_piece, state_board)
+        exp.action = move
+        exp.reward = reward
+        exp.next_state = (representation.PieceState(next_piece), representation.BoardState(gs))
+        exp.done = gs.over()
+        experiences.append(exp)
 
-            # determine what new_target is based upon the game ending or not
-            if exp.done: # if game is over
-                new_target = exp.reward
-            else: # game is not done! There is still more game to go. So we should also consider FUTURE rewards as part or our (the NN's) understanding of what rewards this move will reap, now, or into the future.
-                max_q_value_of_next_state = max(tai.predict(exp.next_state[0], exp.next_state[1])) # this is the "best Q value" of the NEXT state. Which will ALSO consider the Q-value of the NEXT STATE. And that goes on and on, like recursively. So really, every Q-value, to an extent (controlled by gamma), is also an estimation of future rewards.
-                new_target = reward + (gamma * max_q_value_of_next_state) # gamma here serves as a slider scale, essentially setting "how important" the future rewards are vs. the current IMMEDIATE reward. i.e. if gamma was 0, it wouldn't consider the future at all, would just focus on the reward it got for THIS move only.
 
-            # ask the model to predict again, for this experience's state (let me see the Q-value for each move again)
-            qvalues:list[float] = tai.predict(exp.state[0], exp.state[1])
+    # train on every experience
+    print("Recorded experiences has reached " + str(len(experiences)) + " experiences. Time to train!")
+    for exp in experiences:
 
-            # plug in the target where it belongs
-            qvalues[exp.action] = new_target
+        new_target:float # "new_target" is essentially the 'correct' Q-Value that we want the Neural Network to learn for that particular state and action it did. In other words, we are going to set this to the updated current/future reward blend, plug this value into the prediction array, and then train on it.
 
-            # Now, with this new UPDATED qvalues (well, with only 1 changed), train!
-            tai.train(exp.state[0], exp.state[1], qvalues)
+        # determine what new_target is based upon the game ending or not
+        if exp.done: # if game is over
+            new_target = exp.reward
+        else: # game is not done! There is still more game to go. So we should also consider FUTURE rewards as part or our (the NN's) understanding of what rewards this move will reap, now, or into the future.
+            max_q_value_of_next_state = max(tai.predict(exp.next_state[0], exp.next_state[1])) # this is the "best Q value" of the NEXT state. Which will ALSO consider the Q-value of the NEXT STATE. And that goes on and on, like recursively. So really, every Q-value, to an extent (controlled by gamma), is also an estimation of future rewards.
+            new_target = reward + (gamma * max_q_value_of_next_state) # gamma here serves as a slider scale, essentially setting "how important" the future rewards are vs. the current IMMEDIATE reward. i.e. if gamma was 0, it wouldn't consider the future at all, would just focus on the reward it got for THIS move only.
+
+        # ask the model to predict again, for this experience's state (let me see the Q-value for each move again)
+        qvalues:list[float] = tai.predict(exp.state[0], exp.state[1])
+
+        # plug in the target where it belongs
+        qvalues[exp.action] = new_target
+
+        # Now, with this new UPDATED qvalues (well, with only 1 changed), train!
+        tai.train(exp.state[0], exp.state[1], qvalues)
 
         # dump the memory of experiences
         experiences.clear()
@@ -117,4 +132,5 @@ for epoch in range(0, 1000000000):
     # if game is over, reset game!
     if gs.over():
         print("Game is complete w/ final score of " + str(gs.score()) + "! Resetting.")
+        GameScores.append(gs.score())
         gs = tetris.GameState() # new game!
