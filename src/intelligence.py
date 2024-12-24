eabae94edcb3786cd3cb54cb316dac82ac55ac0f
Tim Hanewich <tahanewich@live.com>
2024-12-24 15:19:39 -0500

    Expanded NN to accomodate full board

--- src/intelligence.py
+++ src/intelligence.py
@@ -24,23 +24,24 @@ class TetrisAI:
             # build input piece portion, followed by some hidden layers
             input_piece = keras.layers.Input(shape=(8,), name="input_piece")
             carry_piece = keras.layers.Dense(32, "relu", name="piece_layer1")(input_piece)
+            carry_piece = keras.layers.Dense(32, "relu", name="piece_layer1")(carry_piece)
 
             # build input board portion, followed by some layers
-            input_board = keras.layers.Input(shape=(10,), name="input_board")
-            carry_board = keras.layers.Dense(100, "relu", name="board_layer1")(input_board)
-            carry_board = keras.layers.Dense(100, "relu", name="board_layer2")(carry_board)
-            carry_board = keras.layers.Dense(100, "relu", name="board_layer3")(carry_board)
-            carry_board = keras.layers.Dense(100, "relu", name="board_layer4")(carry_board)
+            input_board = keras.layers.Input(shape=(200,), name="input_board")
+            carry_board = keras.layers.Dense(256, "relu", name="board_layer1")(input_board)
+            carry_board = keras.layers.Dense(256, "relu", name="board_layer2")(carry_board)
+            carry_board = keras.layers.Dense(256, "relu", name="board_layer3")(carry_board)
+            carry_board = keras.layers.Dense(256, "relu", name="board_layer4")(carry_board)
 
             # combine the two into one layer, followed by some layers
             combined = keras.layers.concatenate([carry_piece, carry_board], name="combined")
-            carry = keras.layers.Dense(256, "relu", name="combined_layer1")(combined)
-            carry = keras.layers.Dense(256, "relu", name="combined_layer2")(carry)
+            carry = keras.layers.Dense(512, "relu", name="combined_layer1")(combined)
+            carry = keras.layers.Dense(512, "relu", name="combined_layer2")(carry)
             carry = keras.layers.Dense(256, "relu", name="combined_layer3")(carry)
             carry = keras.layers.Dense(256, "relu", name="combined_layer4")(carry)
             carry = keras.layers.Dense(128, "relu", name="combined_layer5")(carry)
             carry = keras.layers.Dense(128, "relu", name="combined_layer6")(carry)
-            carry = keras.layers.Dense(32, "relu", name="combined_layer7")(carry)
+            carry = keras.layers.Dense(64, "relu", name="combined_layer7")(carry)
             output = keras.layers.Dense(9, "linear", name="output")(carry) # output of 9 potential moves (shift of 0 to shift of 9)
 
             # construct the model
