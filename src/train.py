773d27fbba452ac23531fd7f2fa34eb34933c97e
Tim Hanewich <tahanewich@live.com>
2024-12-26 16:58:50 -0500

    Rewrote training script

--- src/train.py
+++ src/train.py
@@ -1,12 +1,8 @@
 import intelligence
-import sys
-import tools
 import tetris
 import representation
 import random
-import time
-import math
-import collections
+import sys
 
 ### SETTINGS ###
 model_save_path = r"" # if you want to start from a checkpoint, fill this in with the path to the .keras file. If wanting to start from a new NN, leave blank!
@@ -17,9 +13,8 @@ gamma:float = 0.5
 epsilon:float = 0.2
 
 # training config
-batch_size:int = 64 # the number of experiences that will be trained on
+batch_size:int = 300 # the number of experiences that will be collected before training starts
 ################
-
 
 # construct/load model
 tai:intelligence.TetrisAI = None
@@ -31,3 +26,100 @@ else:
     print("Constructing new model...")
     tai = intelligence.TetrisAI()  
 
+# variables to track
+experiences_trained:int = 0 # the number of experiences the model has been trained at at any given point
+
+# training loop!
+while True:
+
+    # collect X number of experiences
+    gs = tetris.GameState()
+    experiences:list[intelligence.Experience] = []
+    for ei in range(0, batch_size):
+
+        # print
+        sys.stdout.write("\r" + "Collecting experience " + str(ei+1) + " / " + str(batch_size) + "... ")
+        sys.stdout.flush()
+
+        # generate piece
+        p = tetris.Piece()
+
+        # get piece & board representation
+        repr_piece:list[int] = representation.PieceState(p)
+        repr_board:list[int] = representation.BoardState(gs)
+
+        # select what move to play
+        move:int
+        if random.random() < epsilon:
+            if p.width == 2: # one shape
+                move = random.randint(0, 8)
+            elif p.width == 3: # most shapes
+                move = random.randint(0, 7)
+            elif p.width == 4: # one shape
+                move = random.randint(0, 6)
+        else:
+            predictions:list[float] = tai.predict(repr_piece, repr_board)
+            move = predictions.index(max(predictions)) # select the index with the highest Q-Value (highest expected reward)
+
+        # Execute the move
+        IllegalMovePlayed:bool = False
+        MoveReward:float = 0.0
+        try:
+            MoveReward = gs.drop(p, move)
+        except tetris.InvalidShiftException as ex:
+            IllegalMovePlayed = True
+            MoveReward = -5.0 # penalize for illegal moves
+        except Exception as ex:
+            print("Unhandled exeption in move execution: " + str(ex))
+            input("Press enter key to continue, if you want to.")
+
+        # create a random next piece for the sake of the "next state"
+        np:tetris.Piece = tetris.Piece()
+        np.randomize()
+
+        # store this experience
+        exp:intelligence.Experience = intelligence.Experience()
+        exp.state = (repr_piece, repr_board) # the situation that was presented to the neural network
+        exp.action = move # the decision the neural network made (or was made by random chance via epsilon)
+        exp.reward = MoveReward # the reward that was given from that move
+        exp.next_state = (representation.PieceState(np), representation.BoardState(gs)) # the next state
+        exp.done = gs.over() or IllegalMovePlayed # if the game is completed or terminated via it finishing or an illegal move being played
+
+        # if the game is over or an illegal move was played, reset
+        if gs.over() or IllegalMovePlayed:
+            gs = tetris.GameState() # new game!
+    print("experience collection complete!")
+
+    # print average rewards
+    rewards:float = 0.0
+    for exp in experiences:
+        rewards = rewards + exp.reward
+    status:str = "Average reward over those " + str(len(experiences)) + " on model w/ " + str(experiences_trained) + " trained experiences: " + str(round(rewards / len(experiences), 2))
+    print(status)
+
+    # train!
+    for ei in range(0, len(experiences)):
+        exp = experiences[ei]
+
+        # print
+        sys.stdout.write("\r", "Training on experience " + str(ei+1) + " / " + str(len(experiences)) + "... ")
+        sys.stdout.flush()
+
+        # determine new target based on the game ending or not
+        new_target:float
+        if exp.done:
+            new_target = exp.reward
+        else:
+            max_q_of_next_state:float = max(tai.predict(exp.next_state[0], exp.next_state[1]))
+            new_target = exp.reward + (gamma * max_q_of_next_state) # blend immediate vs. future rewards
+
+        # ask the model to predict again for this experiences state
+        qvalues:list[float] = tai.predict(exp.state[0], exp.state[1])
+
+        # plug in the new target where it belongs
+        qvalues[exp.action] = new_target
+
+        # now train on the updated qvalues (with 1 value changed)
+        tai.train(exp.state[0], exp.state[1], qvalues)
+        experiences_trained = experiences_trained + 1
+    print("training complete!")
