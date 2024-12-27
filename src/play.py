7f2793a47fe2e5c1d6bea829c21264f1ff9934b2
Tim Hanewich <tahanewich@live.com>
2024-12-26 21:56:22 -0500

    Round reward

--- src/play.py
+++ src/play.py
@@ -15,7 +15,7 @@ while True:
         i:str = input("How many shifts? > ")
         shifts:int = int(i)
         reward:float = gs.drop(p, shifts)
-        print("Reward from move '" + str(reward) + "'")
+        print("Reward from move '" + str(round(reward, 1)) + "'")
 
         # if game over
         if gs.over():
